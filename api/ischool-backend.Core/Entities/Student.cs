d33418b628f9948e7fb152a8e50537690cdb53bb
Radahn <kwakuaffram@gmail.com>
2025-02-15 23:54:29 +0000

    Make Student entity properties required for better validation

--- api/ischool-backend.Core/Entities/Student.cs
+++ api/ischool-backend.Core/Entities/Student.cs
@@ -6,15 +6,15 @@ public class Student
 {
     public Guid Id { get; set; }
 
-    public string StudentId { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public required string StudentId { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
     public string FullName => $"{FirstName} {LastName}";
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
-    public DateTime EnrollmentDate { get; set; }
-    public string Address { get; set; }
-    public string Department { get; set; }
+    public required string Email { get; set; }
+    public required string PhoneNumber { get; set; }
+    public required DateTime EnrollmentDate { get; set; }
+    public required string Address { get; set; }
+    public required string Department { get; set; }
     public decimal GPA { get; set; }
 
     //TODO: Navigation properties
