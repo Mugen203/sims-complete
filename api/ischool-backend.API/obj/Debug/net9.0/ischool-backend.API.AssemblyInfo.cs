79b5337c6436b2146f29afe1d5a19a5417450a08
Radahn <kwakuaffram@gmail.com>
2025-02-16 02:55:16 +0000

    Update launch settings and project structure for improved organization

--- api/ischool-backend.API/obj/Debug/net9.0/ischool-backend.API.AssemblyInfo.cs
+++ api/ischool-backend.API/obj/Debug/net9.0/ischool-backend.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ischool-backend.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d33418b628f9948e7fb152a8e50537690cdb53bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ischool-backend.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ischool-backend.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
